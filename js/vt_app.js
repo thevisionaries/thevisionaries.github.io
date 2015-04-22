02f5e49dde47b17868e7b347182ecc7fedd9d892
roconn14 <roconn14@gmail.com>
2015-04-22 17:19:21 -0400

    Vision Test & Algorithm Update
    New algorithm structure and working php sending mechanism. Updated
    vision test page to work with new algorithm. Changed css slightly to
    center carousel images.

--- js/vt_app.js
+++ js/vt_app.js
@@ -1,33 +1,49 @@
-angular.module('vtApp', ['ngTouch', 'ng']);
-var visionExam = angular.module('vtApp', ['ngTouch', 'ng']);
-visionExam.controller('counts', ['$scope', function($scope){
-    $scope.setCount = function(hide, lc, rc, lc2, rc2, c1, c2, fc, last, last2)
-    {
-        hide = true;
-        lc = false;
-        rc = false;
-        lc2 = false;
-        rc2 = false;
-        c1 = 5;
-        c2 = 3;
-        fc = 0;
-        last = 0;
-        last2 = 0;
-    }
-    $scope.leftCount = function (count, lastclicked, leftcounter)
-    {
-        count = count - 1;
-        lastclicked = 1;
-        leftcounter = true;
-    }
-    $scope.rightCount = function (count, lastclicked, rightcounter)
-    {
-        count = count + 1;
-        lastclicked = 0;
-        rightcounter = true;
-    }
-    $scope.getCount = function(count, lastclicked)
-    {
-        return count + lastclicked;
-    }
-}]);
+angular.module("mainModule", [])
+	.controller("mainController", function ($scope, $http, jsonFilter)
+	{
+		$scope.setCount = function(hide, lc, rc, lc2, rc2, c1, c2, fc, last, last2)
+		{
+			hide = true;
+			lc = false;
+			rc = false;
+			lc2 = false;
+			rc2 = false;
+			c1 = 5;
+			c2 = 3;
+			fc = 0;
+			last = 0;
+			last2 = 0;
+		};
+		
+		$scope.leftCount = function (count, lastclicked, leftcounter)
+		{
+			count = count - 1;
+			lastclicked = 1;
+			leftcounter = true;
+		};
+		
+		$scope.rightCount = function (count, lastclicked, rightcounter)
+		{
+			count = count + 1;
+			lastclicked = 0;
+			rightcounter = true;
+		};
+		
+		$scope.getCount = function(count, lastclicked)
+		{
+			return count + lastclicked;
+		};
+		
+		var logResult = function (str, data, status, headers)
+		{
+			return str + "\n\n" +
+				"data: " + data + "\n\n" +
+				"status: " + status + "\n\n" +
+				"headers: " + jsonFilter(headers());
+		};
+
+		$scope.postCall = function (postData) {
+			$scope.postData = postData;
+			$http.post("server.php", {'postData': $scope.postData});
+		};
+	});
